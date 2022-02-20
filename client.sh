f5c72523afe60f1d692c322463527015171bc94f
Mathura Sothinathan <30644171+mathurasothi@users.noreply.github.com>
2022-02-20 15:04:07 -0500

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#Run script for client distributed as part of 
-#Assignment 1
-#Computer Networks (CS 456)
+
 #Number of parameters: 5
 #Parameter:
 #    $1: <server_address>
@@ -11,16 +9,5 @@
 #    $4: <req_code>
 #    $5: <file_received>
 
-#Uncomment/update exactly one of the following commands depending on your implementation
-
-#For C/C++ implementation
-#./client $1 $2 "$3" $4 "$5"
-
-#For Java implementation
-#java client $1 $2 "$3" $4 "$5"
-
-#For Python implementation
+#Run python scripts
 python client.py $1 $2 "$3" $4 "$5"
-
-#For Ruby implementation
-#ruby client.rb $1 $2 "$3" $4 "$5"
